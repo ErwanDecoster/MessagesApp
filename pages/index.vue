f2ff4512aa89953662e37e78ec778c1c01cc7d2f
Erwan Decoster <wanerd2@gmail.com>
2023-10-22 20:03:38 +0200

    add of a doto list of comming feature on the home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col pt-12 gap-12 max-w-4xl mx-auto sm:pt-0 sm:grid sm:grid-cols-2 min-h-[calc(100vh-2rem)]">
+  <div class="flex flex-col my-24 gap-12 max-w-4xl mx-auto sm:pt-0 sm:grid sm:grid-cols-2 min-h-[calc(100vh-2rem)]">
     <div class="md:px-6 flex flex-col justify-center items-center">
       <p class="text-2xl text-left text-[#E7D7C1] font-bold">Messages App</p>
     </div>
@@ -10,7 +10,7 @@
         <NuxtLink to="/register" class="btn-primary" >Crée un compte</NuxtLink>
       </div>
     </div>
-    <div class="col-span-2 md:px-6">
+    <div class="col-span-2 grid gap-12 md:px-6">
       <div class="bg-[#E7D7C1] rounded-3xl p-4 grid gap-2 mx-auto">
         <h2 class="text-xl">Qu'es ce que Messages App ?</h2>
         <p>MessagesApp est une application de messagerie basique conçue pour simplifier la communication entre les utilisateurs.</p>
@@ -30,6 +30,26 @@
           Avec MessagesApp, la messagerie instantanée n'a jamais été aussi simple et directe. Essayez-la dès aujourd'hui pour une communication fluide et efficace.
           <NuxtLink to="/register" class="btn-secondary inline-block w-fit" >Crée un compte</NuxtLink>
         </p>
+      </div>
+      <div class="bg-[#E7D7C1] rounded-3xl p-4 grid gap-2 mx-auto">
+        <h2 class="text-xl">Fonctionnalités à venir 🚀</h2>
+        <p>Messages App est en cours de développement. Des bugs ou comportements étranges peuvent survenir. Si cela vous arrive, merci de me les signaler à : <a class="text-blue-600" href="mailto:contact@erwan-decoster.com">contact@erwan-decoster.com</a></p>
+        <ul class="list-disc list-inside">
+          <li>🚧Correction de bugs sur la timeline des messages.</li>
+          <li>🚧Amélioration de la responsabilité (le site sera plus facilement utilisable sur mobile).</li>
+          <li>⚡Ajout des paramètres de compte.</li>
+          <ol class="ml-4 list-disc list-inside">
+            <li>⚡Possibilité de ne pas référencer son compte. Il sera impossible pour d'autres utilisateurs de vous trouver avec votre numéro de téléphone.</li>
+            <li>⚡Possibilité de choisir entre plus de thèmes.</li>
+            <li>Possibilité de désactiver les notifications.</li>
+          </ol>
+          <li>Possibilité d'installer le site sur votre écran d'accueil.</li>
+          <li>Ajout d'un système de notification.</li>
+          <li>Possibilité d'activer ou non les notifications d'une conversation.</li>
+          <li>Ajout de la prise en charge des images.</li>
+          <li>Ajout de réponses attachées à un message.</li>
+          <li>Choix entre plusieurs thèmes pour chaque conversation.</li>
+        </ul>
       </div>
     </div>
   </div>
